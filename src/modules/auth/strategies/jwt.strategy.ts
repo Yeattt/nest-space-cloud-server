8624c81fe61af3c8252b4804f2a03ae6a140d626
Jefferson <nekz.8896@gmail.com>
2025-06-06 13:54:19 -0500

    Unauthorized message changed

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -28,7 +28,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
 
     if (!user) {
-      throw new UnauthorizedException(`User not found`);
+      throw new UnauthorizedException(`Invalid token`);
     };
 
     return user;
