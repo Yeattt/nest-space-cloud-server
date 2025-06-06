8fa79fc7fac7aaf0fe4c2e599557266b2a05c42f
Jefferson <nekz.8896@gmail.com>
2025-06-06 13:37:01 -0500

    Prisma service added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { FilesModule } from './modules/files/files.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [FilesModule, AuthModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
