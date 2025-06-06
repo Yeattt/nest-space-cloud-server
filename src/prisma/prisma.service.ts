4c7d93ef30989196cc590e4d7cb96edbcb98811b
Jefferson <nekz.8896@gmail.com>
2025-06-06 14:14:31 -0500

    Bugs in methods fixed

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,10 +4,10 @@ import { PrismaClient } from 'generated/prisma';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   public async onModuleInit(): Promise<void> {
-    await this.$connect;
+    await this.$connect();
   };
 
   public async onModuleDestroy(): Promise<void> {
-    await this.$disconnect;
+    await this.$disconnect();
   };
 }
