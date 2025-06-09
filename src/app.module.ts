3a59f33625b13ed11e11228d83085969b4983296
Jefferson <nekz.8896@gmail.com>
2025-06-09 16:07:40 -0500

    Directories and Auth modules imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { FilesModule } from './modules/files/files.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { DirectoriesModule } from './modules/directories/directories.module';
+import { FilesModule } from './modules/files/files.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [FilesModule, AuthModule],
+  imports: [AuthModule, DirectoriesModule, FilesModule,],
   providers: [PrismaService],
 })
 export class AppModule {}
